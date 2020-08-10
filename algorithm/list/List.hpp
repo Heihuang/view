049eb9d88a049c17fffd056afe26aa90ab634ec4
zhoubing <zhoubing@zuoyebang.com>
2020-08-10 12:56:37 +0800

    add code

--- algorithm/list/List.hpp
+++ algorithm/list/List.hpp
@@ -204,6 +204,31 @@ ListNode* reverseList(ListNode* head)
 }
 
 //面试题7:合并两个有序链表
+//定义头结点
+//若l1指向的结点值 < l2 指向的结点值，则将l1链接到头结点的next位置
+//否则将l2链接到头结点的next位置
+//循环进行，直至l1或l2为NULL
+//最后，将l1或l2中剩下的部分，链接到头结点后面
+ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+    ListNode* head = new ListNode();
+    ListNode* ret = head;
+    while (l1 != NULL && l2 != NULL) {
+        //若l1指向的结点值 < l2 指向的结点值，则将l1链接到头结点的next位置
+        if (l1->value_ < l2->value_) {
+            head->next_ = l1;
+            l1 = l1->next_;
+        } else {
+            //否则将l2链接到头结点的next位置
+            head->next_ = l2;
+            l2 = l2->next_;
+        }
+        head = head->next_;
+    }
+    //将l1或l2中剩下的部分，链接到头结点后面
+    head->next_ = l1 == NULL ? l2 : l1;
+    return ret->next_;
+}
+
 //面试题8:两个链表的第一个公共节点
 //面试题9:圆圈中最后剩下的数字
 //面试题10:复杂链表的复制
