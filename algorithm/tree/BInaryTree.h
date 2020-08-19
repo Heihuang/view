48461ad2c4bd114d5c4ab49ed9a4cd0bc863a4ef
杨丽娜 <yanglina@yanglinadeMacBook-Pro.local>
2020-08-19 23:29:28 +0800

    add code

--- algorithm/tree/BInaryTree.h
+++ algorithm/tree/BInaryTree.h
@@ -1,5 +1,7 @@
 #ifndef _BINARY_TREE_H_
 #define _BINARY_TREE_H_
+#include <stdio.h>
+
 struct BInaryTreeNode
 {
     /* data */
@@ -7,5 +9,40 @@ struct BInaryTreeNode
     BInaryTreeNode* left;
     BInaryTreeNode* right;
 };
-  
+//前序
+void PreOrderTraverse(BInaryTreeNode* root)
+{
+    if (root == nullptr)
+    {
+        /* code */
+        return;
+    }
+    printf("v=%d", root->value);
+    PreOrderTraverse(root->left);
+    PreOrderTraverse(root->right);
+}
+//中序
+void PreOrderTraverse(BInaryTreeNode* root)
+{
+    if (root == nullptr)
+    {
+        /* code */
+        return;
+    }
+    PreOrderTraverse(root->left);
+    printf("v=%d", root->value);
+    PreOrderTraverse(root->right);
+}
+//后序  
+void PreOrderTraverse(BInaryTreeNode* root)
+{
+    if (root == nullptr)
+    {
+        /* code */
+        return;
+    }
+    printf("v=%d", root->value);
+    PreOrderTraverse(root->left);
+    PreOrderTraverse(root->right);
+}
 #endif
---
+++ algorithm/tree/从上往下打印二叉树.hpp
@@ -0,0 +1,33 @@
+//题目：从上往下打印出二叉树的每个节点，同一层的节点按照从左到右的顺序打印
+//解题要点：每一次打印一个节点的时候，如果该节点有子节点，则把该节点的子节点放到一个队列的末尾。
+//接下来到队列的头部取出最早进入队列的节点，重复前面的打印操作，直到队列中的所有节点都被打印出来为止
+#include "BInaryTree.h"
+#include <deque>
+void PrintFropTopToBottom(BInaryTreeNode* root)
+{
+    if (!root)
+    {
+        return;
+    }
+    std::deque<BInaryTreeNode*> dt;
+    dt.push_back(root);
+    while (dt.empty())
+    {
+        /* code */
+        BInaryTreeNode* node = dt.front();
+        dt.pop_front();
+        printf("v=%d", node->value);
+        //如果有左子树节点，将左子树节点放入队列
+        if (node->left)
+        {
+            dt.push_back(node->left);
+        }
+        //如果有右子树节点，将右子树节点放入队列
+        if (node->right)
+        {
+            dt.push_back(node->right);
+        }
+        
+    }
+    
+}
