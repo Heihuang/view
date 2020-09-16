f28de9205d055cc1c8167762d6e6696555de837a
zhoubing <zhoubing@zuoyebang.com>
2020-09-16 20:53:26 +0800

    add codes

---
+++ algorithm/stack/队列的最大值.hpp
@@ -0,0 +1,39 @@
+/*
+题目：请定义一个队列并实现函数 max_value 得到队列里的最大值，要求函数max_value、push_back 和 pop_front 的均摊时间复杂度都是O(1)。
+若队列为空，pop_front 和 max_value 需要返回 -1
+解法：从队列尾部插入元素时，我们可以提前取出队列中所有比这个元素小的元素，使得队列中只保留对结果有影响的数字。这样的方法等价于要求维持队列单
+调递减，即要保证每个元素的前面都没有比它小的元素。
+*/
+
+class MaxQueue {
+    queue<int> q;
+    deque<int> d;
+public:
+    MaxQueue() {
+    }
+    
+    int max_value() {
+        if (d.empty())
+            return -1;
+        return d.front();
+    }
+    
+    void push_back(int value) {
+        while (!d.empty() && d.back() < value) {
+            d.pop_back();
+        }
+        d.push_back(value);
+        q.push(value);
+    }
+    
+    int pop_front() {
+        if (q.empty())
+            return -1;
+        int ans = q.front();
+        if (ans == d.front()) {
+            d.pop_front();
+        }
+        q.pop();
+        return ans;
+    }
+};
